b5e5689eadaba227568b2c82bc06bbb4f16e3a1d
Cindy Ly <lycindy00@gmail.com>
2017-03-02 23:42:28 +0000

    add more students

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+$("#studentButton").click(function() {
+var Code2College_Students = ["Marlene", "Andrea", "Sophie", "Vanessa", "Natalia","Cindy", "Yasmeen"];
+
+var student_index =Math.floor(Math.random()*Code2College_Students.length);
+$("#studentDisplay").append(Code2College_Students[student_index]);
+
+});
+
+
+$("#submitButton").click(function(){
+ Code2College_Students.push($("#name").val());
+});
+
+
+$("#teacherButton").click(function() {
+    var Code2College_Educators = ["Stephanie", "Chris", "Jon","Matt"];
+    var teacher_index = Math.floor(Math.random()*Code2College_Educators.length);
+    $("#teacherDisplay").append(Code2College_Educators[teacher_index]);
+});
 
 
 });
